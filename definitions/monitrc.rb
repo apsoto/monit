08985988d35155166917c28af187634c91283d6d
Cameron Johnston <cameron@needle.com>
2012-05-30 11:45:23 -0600

    make source and cookbook parameters overridable

--- definitions/monitrc.rb
+++ definitions/monitrc.rb
@@ -1,12 +1,12 @@
 # reload      Reload monit so it notices the new service.  :delayed (default) or :immediately.
-define :monitrc, :enable => true, :reload => :delayed do
+define :monitrc, :enable => true, :reload => :delayed, :source => "#{params[:name]}.conf.erb", :cookbook => "monit" do
   if params[:enable]
     template "/etc/monit/conf.d/#{params[:name]}.conf" do
       owner "root"
       group "root"
       mode 0644
-      source "#{params[:name]}.conf.erb"
-      cookbook "monit"
+      source params[:source]
+      cookbook params[:cookbook]
       variables params
       notifies :restart, resources(:service => "monit"), params[:reload]
       action :create
