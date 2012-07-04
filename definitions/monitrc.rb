dd933ca6de622f48400ac96c0e4874f532e24236
Robby Grossman <robby@freerobby.com>
2012-07-04 14:03:04 -0400

    Bugfixes: Invalid references to 'name' on Ubuntu 12/Chef 10.4 corrected, and default action explicitly set to :enable to avoid the same problem.

--- definitions/monitrc.rb
+++ definitions/monitrc.rb
@@ -1,18 +1,19 @@
-# reload      Reload monit so it notices the new service.  :delayed (default) or :immediately.
+# reload: Reload monit so it notices the new service.  :delayed (default) or :immediately.
+# action: :enable To create the monitoring config (default), or :disable to remove it.
 define :monitrc, :action => :enable, :reload => :delayed do
-  if params[:enable]
-    template "/etc/monit/conf.d/#{name}.conf" do
+  if params[:action] == :enable
+    template "/etc/monit/conf.d/#{params[:name]}.conf" do
       owner "root"
       group "root"
       mode 0644
-      source "#{name}.conf.erb"
+      source "#{params[:name]}.conf.erb"
       cookbook "monit"
       variables params
       notifies :restart, resources(:service => "monit"), params[:reload]
       action :create
     end
   else
-    template "/etc/monit/conf.d/#{name}.conf" do
+    template "/etc/monit/conf.d/#{params[:name]}.conf" do
       action :delete
       notifies :restart, resources(:service => "monit"), params[:reload]
     end
