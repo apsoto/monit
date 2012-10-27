9053131fcafeb2be580510fd07b64d659b9a42a9
Greg Thornton <xdissent@me.com>
2012-10-26 21:10:23 -0500

    Clean up handler.

--- files/default/monit_handler.rb
+++ files/default/monit_handler.rb
@@ -8,16 +8,21 @@ class MonitHandler < Chef::Handler
     @poll_period = opts[:poll_period] || 1
   end
 
+  def success
+    Chef::Log.info "Monit monitor all succeeded"
+  end
+
+  def failure
+    Chef::Log.warn "Monit monitor all failed"
+  end
+
   def report
     if system("ps -e | grep `cat /var/run/monit.pid`")
-      did_monitor = system("monit monitor all")
-      if did_monitor
-        Chef::Log.info "Monit monitor all succeeded"
-      else
-        Chef::Log.info "Monit monitor all failed, waiting #{@poll_period} seconds before trying again"
-        did_monitor = system("sleep #{@poll_period} && monit monitor all")
-      end
-      Chef::Log.info("Monit monitor all failed") unless did_monitor
+      return success if system("monit monitor all")
+      failure
+      Chef::Log.warn "Waiting #{@poll_period} seconds before trying again..."
+      return success if system("sleep #{@poll_period} && monit monitor all")
+      failure
     end
   end
 
