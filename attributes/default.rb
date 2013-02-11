a4e01349d337108390151a6ffb98ac4ae4e90369
Lloyd Philbrook <lloyd@firebellydesign.com>
2013-02-11 19:07:07 +0800

    added customization to notify alert
    keyword attribute for noise keywords (only, on, not)
    event attribute takes string or array

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,4 +1,6 @@
 default[:monit][:notify_email]          = "notify@example.com"
+default[:monit][:notify_keyword] 		= "NOT ON"
+default[:monit][:notify_event] 			= [ 'action', 'instance', 'pid', 'ppid' ]
 
 default[:monit][:poll_period]           = 60
 default[:monit][:poll_start_delay]      = 120
