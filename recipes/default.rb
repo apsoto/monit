798bd3ecaed6e1d3c7966bdf64aabaecb3df4d8c
JJ Asghar <jj.asghar@peopleadmin.com>
2013-10-29 11:10:24 -0500

    updated to 5.6, and added bulding from src

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,4 +1,32 @@
-package "monit"
+package "monit" do
+    action :remove
+end
+
+remote_file "#{Chef::Config[:file_cache_path]}/monit-5.6.tar.gz" do
+  source "http://mmonit.com/monit/dist/monit-5.6.tar.gz"
+  owner "root"
+  group "root"
+  mode 0644
+end
+
+script "compile_monit" do
+  interpreter "bash"
+  user "root"
+  cwd "#{Chef::Config[:file_cache_path]}"
+  creates "/usr/local/share/man/man1/monit.1"
+  code <<-EOH
+    STATUS=0
+    tar xvzf monit-5.6.tar.gz || STATUS=1
+    cd monit-5.6 || STATUS=1
+    ./configure --without-pam --without-ssl || STATUS=1
+    make || STATUS=1
+    make install || STATUS=1
+    cd #{Chef::Config[:file_cache_path]}
+    rm -rf monit-5.6*
+    exit $STATUS
+  EOH
+end
+
 
 if platform?("ubuntu")
   cookbook_file "/etc/default/monit" do
@@ -23,7 +51,7 @@ directory "/etc/monit/conf.d/" do
   recursive true
 end
 
-template "/etc/monit/monitrc" do
+template "/etc/monitrc" do
   owner "root"
   group "root"
   mode 0700
