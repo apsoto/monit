87ec0fe066583bd58b73052cfe281ede5f80d64e
Jonathon W. Marshall <jonathon@bnotions.com>
2013-12-05 04:47:58 +0000

    FC019: Access node attributes in a consistent manner

--- attributes/default.rb
+++ attributes/default.rb
@@ -6,7 +6,7 @@ default[:monit][:poll_period]           = 60
 default[:monit][:poll_start_delay]      = 120
 
 default[:monit][:mail_format][:subject] = "$SERVICE $EVENT"
-default[:monit][:mail_format][:from]    = "monit@#{node['fqdn']}"
+default[:monit][:mail_format][:from]    = "monit@#{node[:fqdn]}"
 default[:monit][:mail_format][:message]    = <<-EOS
 Monit $ACTION $SERVICE at $DATE on $HOST: $DESCRIPTION.
 Yours sincerely,
